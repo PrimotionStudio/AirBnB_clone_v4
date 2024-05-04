097d82c7d597ae1ad613d42e5dbc86cf6df7e5f0
Princewill Oriyebaka <oriyebakap6@gmail.com>
2024-05-04 15:56:43 +0100

    amenities bug fixed

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    let amenity_ids = [];
-    $('input[type=checkbox]').change(function() {
-        let amenity_id = $(this).data('id');
-        if ($(this).is(':checked')) {
-            amenity_ids.push(amenity_id);
-        } else {
-            let i = amenity_ids.indexOf(amenity_id);
-            if (i !== -1) {
-                amenity_ids.splice(i, 1);
-            }
-            amenity_ids = amenity_ids.filter(function(item) {
-                return item !== amenity_id;
-            });
-        }
-        update_amenities();
-    });
-    function update_amenities() {
-        var a_text = amenity_ids.length ? amenity_ids.join(', ') : 'None';
-        $('.amenities h4').text(a_text);
-    }
-    
+  let amenities = {};
+  let i = 1;
+  $('input[type=checkbox]').change(function () {
+      let amenity_id = $(this).data('id');
+      let amenity_name = $(this).data('name') || i++;
+      console.log($(this).data('name'));
+      if (this.checked) {
+          console.log("Check");
+          amenities[amenity_id] = amenity_name;
+      } else {
+          console.log("Uncheck");
+          delete amenities[amenity_id];
+      }
+      console.log(amenities);
+      update_amenities();
+  });
+  function update_amenities() {
+      amty = Object.values(amenities);
+      var a_text = (amty.length > 0) ? amty.join(', ') : '';
+      $('.amenities h4').text(a_text);
+  }
 });
 const url = 'http://0.0.0.0:5001/api/v1/status/';
 document.addEventListener('DOMContentLoaded', function () {
